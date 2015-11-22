a8d758ba9e7314de9208e4b7d4a5307d1b4fefd0
netblue30 <netblue30@yahoo.com>
2015-11-22 13:15:13 -0500

    feature test

---
+++ test/features/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Feature testing
+#
+
+echo "TESTING: 1.1. new /boot"
+./1.1.exp
+
+echo "TESTING: 1.2. new /proc"
+./1.2.exp
+
+echo "TESTING: 1.5. PID namespace"
+./1.5.exp
+
+echo "TESTING: 1.6 new /var/log"
+./1.6.exp
+
+echo "TESTING: 1.7 new /var/tmp"
+./1.7.exp
+
+#echo "TESTING: "
+#./1..exp
+

be116e6ea6ecd02d0cfaa79c529d4e6b7c31a353
Reiner Herrmann <reiner@reiner-h.de>
2015-12-16 23:25:45 +0100

    Embed deterministic timestamp in manpages
    If the build system supports SOURCE_DATE_EPOCH [1], use this timestamp
    for the manpages, instead of using the current date.
    This will generate reproducible manpages.
    Fall back to old behavior if variable is not set.
    
    Also normalize locale and timezone.
    
    [1]: https://reproducible-builds.org/specs/source-date-epoch/

--- mkman.sh
+++ mkman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed "s/VERSION/$1/g" $2 > $3
-MONTH=`date +%b`
+MONTH=`LC_ALL=C date -u --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%b`
 sed -i "s/MONTH/$MONTH/g" $3
-YEAR=`date +%Y`
+YEAR=`LC_ALL=C date -u --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y`
 sed -i "s/YEAR/$YEAR/g" $3

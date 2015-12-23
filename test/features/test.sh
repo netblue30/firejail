686bc51e38b0718651e82818871b0bf1451c8b21
netblue30 <netblue30@yahoo.com>
2015-12-23 12:38:13 -0500

    0.9.36 testing

--- test/features/test.sh
+++ test/features/test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+OVERLAY="overlay"
+
+while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
+    case "$1" in
+    --nooverlay)
+    	OVERLAY=""
+	;;
+    --help)
+    	echo "./test.sh [--nooverlay|--help] | grep TESTING"
+    	exit
+    	;;
+    esac
+    shift       # Check next set of parameters.
+done
+
+
+
 
 #
 # Feature testing
@@ -6,57 +23,57 @@
 
 # Default features
 echo "TESTING: 1.1 disable /boot"
-./1.1.exp
+./1.1.exp $OVERLAY
 
 echo "TESTING: 1.2 new /proc"
-./1.2.exp
+./1.2.exp $OVERLAY
 
 echo "TESTING: 1.4 mask other users"
-./1.4.exp
+./1.4.exp $OVERLAY
 
 echo "TESTING: 1.5 PID namespace"
-./1.5.exp
+./1.5.exp $OVERLAY
 
 echo "TESTING: 1.6 new /var/log"
-./1.6.exp
+./1.6.exp $OVERLAY
 
 echo "TESTING: 1.7 new /var/tmp"
-./1.7.exp
+./1.7.exp $OVERLAY
 
 echo "TESTING: 1.8 disable /etc/firejail and ~/.config/firejail"
-./1.8.exp
+./1.8.exp $OVERLAY
 
 echo "TESTING: 1.10 disable /selinux"
-./1.10.exp
+./1.10.exp $OVERLAY
 
 # networking features
 echo "TESTING: 2.1 hostname"
-./2.1.exp
+./2.1.exp $OVERLAY
 
 echo "TESTING: 2.2 DNS"
-./2.2.exp
+./2.2.exp $OVERLAY
 
 echo "TESTING: 2.3 mac-vlan"
-./2.3.exp
+./2.3.exp $OVERLAY
 
 echo "TESTING: 2.4 bridge"
-./2.4.exp
+./2.4.exp $OVERLAY
 
 echo "TESTING: 2.5 interface"
-./2.5.exp
+./2.5.exp $OVERLAY
 
 echo "TESTING: 2.6 Default gateway"
-./2.6.exp
+./2.6.exp $OVERLAY
 
 echo "TESTING: 3.1 tmpfs"
-./3.1.exp
+./3.1.exp $OVERLAY
 
 echo "TESTING: 3.2 read-only"
-./3.2.exp
+./3.2.exp $OVERLAY
 
 echo "TESTING: 3.3 blacklist"
-./3.3.exp
+./3.3.exp $OVERLAY
 
 echo "TESTING: 3.4 whitelist"
-./3.4.exp
+./3.4.exp $OVERLAY
 

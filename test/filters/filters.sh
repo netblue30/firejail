ea774178f6d8f613c96c0d001fed200155b704b7
netblue30 <netblue30@yahoo.com>
2016-04-30 07:31:21 -0400

    added make test-filters

---
+++ test/filters/filters.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# This file is part of Firejail project
+# Copyright (C) 2014-2016 Firejail Authors
+# License GPL v2
+
+export MALLOC_CHECK_=3
+export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
+
+echo "TESTING: noroot (test/filters/noroot.exp)"
+./noroot.exp
+
+echo "TESTING: capabilities (test/filters/caps.exp)"
+./caps.exp
+
+echo "TESTING: protocol (test/filters/protocol.exp)"
+./protocol.exp
+
+echo "TESTING: seccomp bad empty (test/filters/seccomp-bad-empty.exp)"
+./seccomp-bad-empty.exp
+
+echo "TESTING: seccomp debug  (test/filters/seccomp-debug.exp)"
+./seccomp-debug.exp
+
+echo "TESTING: seccomp errno (test/filters/seccomp-errno.exp)"
+./seccomp-errno.exp
+
+echo "TESTING: seccomp su (test/filters/seccomp-su.exp)"
+./seccomp-su.exp
+
+echo "TESTING: seccomp ptrace (seccomp-ptrace.exp)"
+./seccomp-ptrace.exp
+
+echo "TESTING: seccomp chmod - seccomp lists (test/filters/seccomp-chmod.exp)"
+./seccomp-chmod.exp
+
+echo "TESTING: seccomp chmod profile - seccomp lists (test/filters/seccomp-chmod-profile.exp)"
+./seccomp-chmod-profile.exp
+
+echo "TESTING: seccomp empty (test/filters/seccomp-empty.exp)"
+./seccomp-empty.exp
+
+echo "TESTING: seccomp bad empty (test/filters/seccomp-bad-empty.exp)"
+./seccomp-bad-empty.exp
+
+echo "TESTING: seccomp dual filter (test/filters/seccomp-dualfilter.exp)"
+./seccomp-dualfilter.exp
+
+

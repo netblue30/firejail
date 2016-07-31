369dd18d206945080780f35b502f61b88448bab1
Reiner Herrmann <reiner@reiner-h.de>
2016-07-31 21:10:40 +0200

    Simplify installation of profiles and manpages

--- mketc.sh
+++ mketc.sh
@@ -2,23 +2,7 @@
 rm -fr .etc
 mkdir .etc
 
-result=$(echo $1 | sed 's/\//\\\//g')
-echo $result
-
-FILES=`ls etc/*.profile`
-for file in $FILES
+for file in etc/*.profile etc/*.inc etc/*.net;
 do
-	sed "s/\/etc\/firejail/$result\/firejail/g" $file > .$file
-done
-
-FILES=`ls etc/*.inc`
-for file in $FILES
-do
-	sed "s/\/etc\/firejail/$result\/firejail/g" $file > .$file
-done
-
-FILES=`ls etc/*.net`
-for file in $FILES
-do
-	sed "s/\/etc\/firejail/$result\/firejail/g" $file > .$file
+	sed "s;/etc/firejail;$1/firejail;g" $file > .$file
 done

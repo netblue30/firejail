030f80b4f236a48bffc2a78f1db1675c1d18fa67
netblue <netblue@debian>
2016-08-16 09:59:33 -0400

    merging overlay

--- src/firejail/firejail.h
+++ src/firejail/firejail.h
@@ -223,8 +223,8 @@ extern int arg_debug_whitelists;	// print debug messages for whitelists
 extern int arg_nonetwork;	// --net=none
 extern int arg_command;	// -c
 extern int arg_overlay;		// overlay option
+extern int arg_overlay_keep;	// place overlay diff in a known directory
 extern int arg_overlay_reuse;	// allow the reuse of overlays
-extern int arg_overlay_keep;	// place overlay diff directory in ~/.firejail
 
 extern int arg_seccomp;	// enable default seccomp filter
 

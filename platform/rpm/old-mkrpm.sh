7cf5be4b224429c4b08db55232c438e1b5098042
netblue30 <netblue30@yahoo.com>
2017-01-15 11:57:52 -0500

    centos rpm

--- platform/rpm/old-mkrpm.sh
+++ platform/rpm/old-mkrpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="0.9.44.4"
+VERSION="0.9.44.6"
 rm -fr ~/rpmbuild
 rm -f firejail-$VERSION-1.x86_64.rpm
 
@@ -458,6 +458,9 @@ rm -rf %{buildroot}
 chmod u+s /usr/bin/firejail
 
 %changelog
+* Sun Jan 15 2017  netblue30 <netblue30@yahoo.com> 0.9.44.6-1
+  - security release
+
 * Sat Jan 7 2017 netblue30 <netblue30@yahoo.com> 0.9.44.4-1
   - security release
 

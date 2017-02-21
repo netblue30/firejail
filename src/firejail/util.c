b74d3870246ce03aaff4a1c40867f10aa9d87267
netblue30 <netblue30@yahoo.com>
2017-02-21 10:03:59 -0500

    merge #1100 from zackw: wait_for_other function rewrite

--- src/firejail/util.c
+++ src/firejail/util.c
@@ -605,15 +605,36 @@ void wait_for_other(int fd) {
 		*ptr = '\0';
 	}
 	else {
-		fprintf(stderr, "Error: cannot establish communication with the parent, exiting...\n");
+		fprintf(stderr, "Error: proc %d cannot sync with peer: %s\n",
+			getpid(), ferror(stream) ? strerror(errno) : "unexpected EOF");
+
+		int status = 0;
+		pid_t pid = wait(&status);
+		if (pid != -1) {
+			if (WIFEXITED(status))
+				fprintf(stderr, "Peer %d unexpectedly exited with status %d\n",
+					pid, WEXITSTATUS(status));
+			else if (WIFSIGNALED(status))
+				fprintf(stderr, "Peer %d unexpectedly killed (%s)\n",
+					pid, strsignal(WTERMSIG(status)));
+			else
+				fprintf(stderr, "Peer %d unexpectedly exited "
+					"(un-decodable wait status %04x)\n", pid, status);
+		}
 		exit(1);
 	}
+
 	if (strcmp(childstr, "arg_noroot=0") == 0)
 		arg_noroot = 0;
+	else if (strcmp(childstr, "arg_noroot=1") == 0)
+		arg_noroot = 1;
+	else {
+		fprintf(stderr, "Error: unexpected message from peer: %s\n", childstr);
+		exit(1);
+	}
 
 	fclose(stream);
 }
-
 
 void notify_other(int fd) {
 	FILE* stream;

edaceb584ee0d04c93380e6b3ecd48ba0abe1a46
netblue30 <netblue30@yahoo.com>
2017-03-14 08:59:11 -0400

    fixed handling of .local profile files when the software is installed ~/.local directory

--- src/firejail/profile.c
+++ src/firejail/profile.c
@@ -971,7 +971,8 @@ void profile_read(const char *fname) {
 	FILE *fp = fopen(fname, "r");
 	if (fp == NULL) {
 		// if the file ends in ".local", do not exit
-		char *ptr = strstr(fname, ".local");
+		const char *base = gnu_basename(fname);
+		char *ptr = strstr(base, ".local");
 		if (ptr && strlen(ptr) == 6)
 			return;
 

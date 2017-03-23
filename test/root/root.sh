ad14d091b2babc7a429f922844a8fc1d8523846f
netblue30 <netblue30@yahoo.com>
2017-03-23 15:02:35 -0400

    merge #1100 from zackw: xvfb support in /etc/firejail/firejail.config

--- test/root/root.sh
+++ test/root/root.sh
@@ -82,6 +82,7 @@ echo "TESTING: seccomp chown (test/root/seccomp-chown.exp)"
 #********************************
 echo "TESTING: firejail configuration (test/root/checkcfg.exp)"
 ./checkcfg.exp
+cp ../../etc/firejail.config /etc/firejail/.
 
 echo "TESTING: tmpfs (test/root/option_tmpfs.exp)"
 ./option_tmpfs.exp

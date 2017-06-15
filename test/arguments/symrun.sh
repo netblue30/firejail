80302df0d4e9062de637889c0d9c8c7461070805
Reiner Herrmann <reiner@reiner-h.de>
2017-06-15 15:52:38 +0200

    test: fix symrun test by calling faudit through symlink to firejail

--- test/arguments/symrun.sh
+++ test/arguments/symrun.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 mkdir symtest
-ln -s /usr/bin/firejail symtest/argtest
+ln -s /usr/bin/firejail symtest/faudit
 
-# search for argtest in current directory
+# search for faudit in current directory
 export PATH=$PATH:.
+export FIREJAIL_TEST_ARGUMENTS=yes
 
 echo "TESTING: 2.1 - simple args"
-symtest/argtest arg1 arg2
+symtest/faudit arg1 arg2
 
 # simple quotes, testing spaces in file names
 echo "TESTING: 2.2 - args with space and \""
-symtest/argtest "arg1 tail" "arg2 tail"
+symtest/faudit "arg1 tail" "arg2 tail"
 
 echo "TESTING: 2.3 - args with space and '"
-symtest/argtest 'arg1 tail' 'arg2 tail'
+symtest/faudit 'arg1 tail' 'arg2 tail'
 
 # escaped space in file names
 echo "TESTING: 2.4 - args with space and \\"
-symtest/argtest arg1\ tail arg2\ tail
+symtest/faudit arg1\ tail arg2\ tail
 
 # & char appears in URLs - URLs should be quoted
 echo "TESTING: 2.5 - args with & and \""
-symtest/argtest "arg1&tail" "arg2&tail"
+symtest/faudit "arg1&tail" "arg2&tail"
 
 echo "TESTING: 2.6 - args with & and '"
-symtest/argtest 'arg1&tail' 'arg2&tail'
+symtest/faudit 'arg1&tail' 'arg2&tail'
 
 rm -fr symtest

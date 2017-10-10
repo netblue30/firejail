6b92f685ed60d846419e657827e091cf3329517e
netblue30 <netblue30@yahoo.com>
2017-10-10 12:23:57 -0400

    private-lib

---
+++ test/private-lib/private-lib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This file is part of Firejail project
+# Copyright (C) 2014-2017 Firejail Authors
+# License GPL v2
+
+export MALLOC_CHECK_=3
+export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
+LIST="evince galculator gnome-calculator hexchat kcalc ktorrent leafpad mousepad qbittorrent transmission-gtk transmission-qt xcalc"
+
+
+for app in $LIST; do
+	which $app
+	if [ "$?" -eq 0 ];
+	then
+		echo "TESTING: private-lib $app"
+		./$app.exp
+	else
+		echo "TESTING SKIP: $app not found"
+	fi
+done
+exit	

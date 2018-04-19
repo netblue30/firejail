b6156fdeaa31646689a74384e2f0ac2bf517bd9a
Reiner Herrmann <reiner@reiner-h.de>
2018-04-19 18:51:48 +0200

    mkuid: fall back to default values if no UID_MIN / GID_MIN found
    Closes #1897

--- mkuid.sh
+++ mkuid.sh
@@ -6,15 +6,15 @@ echo "#define FIREJAIL_UIDS_H" >> uids.h
 
 if [ -r /etc/login.defs ]
 then
-	echo "// using values extracted from /etc/login.defs" >> uids.h
 	UID_MIN=`awk '/^\s*UID_MIN\s*([0-9]*).*?$/ {print $2}' /etc/login.defs`
 	GID_MIN=`awk '/^\s*GID_MIN\s*([0-9]*).*?$/ {print $2}' /etc/login.defs`
-	echo "#define UID_MIN $UID_MIN" >> uids.h
-	echo "#define GID_MIN $GID_MIN" >> uids.h
-else
-	echo "// using default values" >> uids.h
-	echo "#define UID_MIN 1000" >> uids.h
-	echo "#define GID_MIN 1000" >> uids.h
 fi
+
+# use default values if not found
+[ -z "$UID_MIN" ] && UID_MIN="1000"
+[ -z "$GID_MIN" ] && GID_MIN="1000"
+
+echo "#define UID_MIN $UID_MIN" >> uids.h
+echo "#define GID_MIN $GID_MIN" >> uids.h
 
 echo "#endif" >> uids.h

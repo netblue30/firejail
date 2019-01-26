198368d6c2b257cd7c4c99337176e744944087e4
netblue30 <netblue30@yahoo.com>
2019-01-26 10:18:50 -0500

    remove duplicated tests from make test-profiles

--- test/profiles/profiles.sh
+++ test/profiles/profiles.sh
@@ -44,21 +44,3 @@ do
 	./test-profile.exp $PROFILE
 done
 
-echo "TESTING: profile syntax (test/profiles/profile_syntax.exp)"
-./profile_syntax.exp
-
-echo "TESTING: profile syntax 2 (test/profiles/profile_syntax2.exp)"
-./profile_syntax2.exp
-
-echo "TESTING: ignore command (test/profiles/ignore.exp)"
-./ignore.exp
-
-echo "TESTING: profile read-only (test/profiles/profile_readonly.exp)"
-./profile_readonly.exp
-
-echo "TESTING: profile read-only links (test/profiles/profile_readonly.exp)"
-./profile_followlnk.exp
-
-echo "TESTING: profile no permissions (test/profiles/profile_noperm.exp)"
-./profile_noperm.exp
-

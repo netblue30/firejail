373d91148d714f7c2fae95aeb6fcb850a215163c
Reiner Herrmann <reiner@reiner-h.de>
2019-02-05 21:49:33 +0100

    use correct struct member for offset
    Fixes: #2381

--- src/fsec-print/print.c
+++ src/fsec-print/print.c
@@ -206,7 +206,7 @@ static void bpf_decode_args(const struct sock_filter *bpf, unsigned int line) {
 				printf("data.syscall-number");
 				syscall_loaded = 1;
 			}
-			else if (bpf->k == offsetof(struct seccomp_data, nr))
+			else if (bpf->k == offsetof(struct seccomp_data, instruction_pointer))
 				printf("data.instruction_pointer");
 			else {
 				int index = bpf->k - offsetof(struct seccomp_data, args);

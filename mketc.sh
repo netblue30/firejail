e8b60d782eccee13fae539732236e0b19111f706
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2020-04-04 19:40:11 +0200

    Speedup the buildsystem
     - replaing 'include /etc/firejail/foobar.inc' with
       'include $(sysconfdir)/firejail/foobar.inc' is useless since 0.9.58
     - onetime calling install with globbing is faster the a loop calling
       install nearly 1000 times

--- mketc.sh
+++ mketc.sh
@@ -3,16 +3,6 @@
 # Copyright (C) 2014-2020 Firejail Authors
 # License GPL v2
 
-rm -fr .etc
-mkdir .etc
-
-for file in etc/*.profile etc/*.inc etc/*.net;
-do
-	sed "s;/etc/firejail;$1/firejail;g" $file > .$file
-done
-
-if [ "x$2" = "xyes" ]
-then
 sed -i -e '
 1i# Workaround for systems where common UNIX utilities are symlinks to busybox.\
 # If this is not your case you can remove --enable-busybox-workaround from\
@@ -23,5 +13,4 @@ noblacklist \${PATH}/su\
 noblacklist \${PATH}/sudo\
 noblacklist \${PATH}/nc\
 noblacklist \${PATH}/crontab\
-' .etc/disable-common.inc
-fi
+' etc/disable-common.inc

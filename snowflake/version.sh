1823729e212cc786b1ff9f6cf7d164366208e982
Derek Denny-Brown <derek.denny-brown@snowflake.com>
2020-10-30 18:12:34 +0000

    Update version

--- snowflake/version.sh
+++ snowflake/version.sh
@@ -1,2 +1,2 @@
-#0.9.62 is the firejail version, last suffix is snowflake identifier
-FIREJAIL_SF_VERSION=0.9.62.3
+#0.9.64 is the firejail version, last suffix is snowflake identifier
+FIREJAIL_SF_VERSION=0.9.64.1

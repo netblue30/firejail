b0b3fd4a0481929aa82166f408d59e3fdc19fe4c
Reiner Herrmann <reiner@reiner-h.de>
2020-11-12 23:14:09 +0100

    ci: test also transmission profile

--- test/profiles/profiles.sh
+++ test/profiles/profiles.sh
@@ -40,7 +40,7 @@ if [ -d "/run/user/$UID" ]; then
 	PROFILES=`ls /etc/firejail/*.profile`
 	echo "TESTING: default profiles installed in /etc"
 else
-	PROFILES=`ls /etc/firejail/fi*.profile /etc/firejail/fl*.profile /etc/firejail/free*.profile`
+	PROFILES=`ls /etc/firejail/transmission*.profile /etc/firejail/fi*.profile /etc/firejail/fl*.profile /etc/firejail/free*.profile`
 	echo "TESTING: small number of default profiles installed in /etc"
 fi
 

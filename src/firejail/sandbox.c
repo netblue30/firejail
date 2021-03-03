4dce7616909c4e3b6a44901c14ac87a0d5655fef
smitsohu <smitsohu@gmail.com>
2021-03-03 22:22:54 +0100

    sandbox setup: postpone fslogger
    postpone writing of log file in order to
    catch filesystem modifications from x11
    functions

--- src/firejail/sandbox.c
+++ src/firejail/sandbox.c
@@ -1026,12 +1026,6 @@ int sandbox(void* sandbox_arg) {
 	fs_resolvconf();
 
 	//****************************
-	// fs post-processing
-	//****************************
-	fs_logger_print();
-	fs_logger_change_owner();
-
-	//****************************
 	// start dhcp client
 	//****************************
 	dhcp_start();
@@ -1078,6 +1072,12 @@ int sandbox(void* sandbox_arg) {
 
 	// save original umask
 	save_umask();
+
+	//****************************
+	// fs post-processing
+	//****************************
+	fs_logger_print();
+	fs_logger_change_owner();
 
 	//****************************
 	// set security filters

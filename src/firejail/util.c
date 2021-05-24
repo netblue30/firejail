netblue30/firejail

commit 825ac9cdc38c4285584e69d6f29102b149914dfe

open files O_CLOEXEC|O_EXCL
Dumb patch that adds O_CLOEXEC to all open/fopen
calls, even where it is obviously pointless.

While at it, also add O_EXCL where it might be
considered useful, for example to clear Coverity
warnings, or on files that subsequently are used
to configure a join sandbox.

Pure defense in depth, this patch should have no
observable effects.

src/firejail/util.c
@@ -298,14 +298,14 @@ int copy_file(const char *srcname, const char *destname, uid_t uid, gid_t gid, m
 	assert(destname);
 
 	// open source
-	int src = open(srcname, O_RDONLY);
+	int src = open(srcname, O_RDONLY|O_CLOEXEC);
 	if (src < 0) {
 		fwarning("cannot open source file %s, file not copied\n", srcname);
 		return -1;
 	}
 
 	// open destination
-	int dst = open(destname, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	int dst = open(destname, O_CREAT|O_WRONLY|O_TRUNC|O_CLOEXEC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (dst < 0) {
 		fwarning("cannot open destination file %s, file not copied\n", destname);
 		close(src);
@@ -348,7 +348,7 @@ void copy_file_as_user(const char *srcname, const char *destname, uid_t uid, gid
 
 void copy_file_from_user_to_root(const char *srcname, const char *destname, uid_t uid, gid_t gid, mode_t mode) {
 	// open destination
-	int dst = open(destname, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	int dst = open(destname, O_CREAT|O_WRONLY|O_TRUNC|O_CLOEXEC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (dst < 0) {
 		fwarning("cannot open destination file %s, file not copied\n", destname);
 		return;
@@ -361,7 +361,7 @@ void copy_file_from_user_to_root(const char *srcname, const char *destname, uid_
 		// drop privileges
 		drop_privs(0);
 
-		int src = open(srcname, O_RDONLY);
+		int src = open(srcname, O_RDONLY|O_CLOEXEC);
 		if (src < 0) {
 			fwarning("cannot open source file %s, file not copied\n", srcname);
 		} else {
@@ -616,7 +616,7 @@ int find_child(pid_t parent, pid_t *child) {
 			perror("asprintf");
 			exit(1);
 		}
-		FILE *fp = fopen(file, "r");
+		FILE *fp = fopen(file, "re");
 		if (!fp) {
 			free(file);
 			continue;
@@ -722,7 +722,7 @@ void update_map(char *mapping, char *map_file) {
 		if (mapping[j] == ',')
 			mapping[j] = '\n';
 
-	fd = open(map_file, O_RDWR);
+	fd = open(map_file, O_RDWR|O_CLOEXEC);
 	if (fd == -1) {
 		fprintf(stderr, "Error: cannot open %s: %s\n", map_file, strerror(errno));
 		exit(EXIT_FAILURE);
@@ -742,9 +742,9 @@ void wait_for_other(int fd) {
 	// wait for the parent to be initialized
 	//****************************
 	char childstr[BUFLEN + 1];
-	int newfd = dup(fd);
+	int newfd = fcntl(fd, F_DUPFD_CLOEXEC, 0);
 	if (newfd == -1)
-		errExit("dup");
+		errExit("fcntl");
 	FILE* stream;
 	stream = fdopen(newfd, "r");
 	*childstr = '\0';
@@ -791,9 +791,9 @@ void wait_for_other(int fd) {
 
 void notify_other(int fd) {
 	FILE* stream;
-	int newfd = dup(fd);
+	int newfd = fcntl(fd, F_DUPFD_CLOEXEC, 0);
 	if (newfd == -1)
-		errExit("dup");
+		errExit("fcntl");
 	stream = fdopen(newfd, "w");
 	fprintf(stream, "arg_noroot=%d\n", arg_noroot);
 	fflush(stream);
@@ -811,7 +811,7 @@ uid_t pid_get_uid(pid_t pid) {
 		exit(1);
 	}
 	EUID_ROOT();				  // grsecurity fix
-	FILE *fp = fopen(file, "r");
+	FILE *fp = fopen(file, "re");
 	if (!fp) {
 		free(file);
 		fprintf(stderr, "Error: cannot open /proc file\n");
@@ -1021,8 +1021,7 @@ void create_empty_file_as_root(const char *fname, mode_t mode) {
 		if (arg_debug)
 			printf("Creating empty %s file\n", fname);
 
-		/* coverity[toctou] */
-		FILE *fp = fopen(fname, "w");
+		FILE *fp = fopen(fname, "wxe");
 		if (!fp)
 			errExit("fopen");
 		SET_PERMS_STREAM(fp, 0, 0, mode);

src/firejail/appimage.c
@@ -67,7 +67,7 @@ void appimage_set(const char *appimage) {
 
 	// find or allocate a free loop device to use
 	EUID_ROOT();
-	int cfd = open("/dev/loop-control", O_RDWR);
+	int cfd = open("/dev/loop-control", O_RDWR|O_CLOEXEC);
 	if (cfd == -1)
 		err_loop();
 	int devnr = ioctl(cfd, LOOP_CTL_GET_FREE);
@@ -78,7 +78,7 @@ void appimage_set(const char *appimage) {
 		errExit("asprintf");
 
 	// associate loop device with appimage
-	int lfd = open(devloop, O_RDONLY);
+	int lfd = open(devloop, O_RDONLY|O_CLOEXEC);
 	if (lfd == -1)
 		err_loop();
 	if (ioctl(lfd, LOOP_SET_FD, ffd) == -1)
@@ -146,7 +146,7 @@ void appimage_mount(void) {
 void appimage_clear(void) {
 	EUID_ROOT();
 	if (devloop) {
-		int lfd = open(devloop, O_RDONLY);
+		int lfd = open(devloop, O_RDONLY|O_CLOEXEC);
 		if (lfd != -1) {
 			if (ioctl(lfd, LOOP_CLR_FD, 0) != -1)
 				fmessage("AppImage detached\n");

src/firejail/bandwidth.c
@@ -22,6 +22,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 #include <net/if.h>
 #include "firejail.h"
 
@@ -119,26 +120,19 @@ static void bandwidth_create_run_file(pid_t pid) {
 	if (asprintf(&fname, "%s/%d-bandwidth", RUN_FIREJAIL_BANDWIDTH_DIR, (int) pid) == -1)
 		errExit("asprintf");
 
-	// if the file already exists, do nothing
-	struct stat s;
-	if (stat(fname, &s) == 0) {
-		free(fname);
-		return;
-	}
-
 	// create an empty file and set mod and ownership
-	/* coverity[toctou] */
-	FILE *fp = fopen(fname, "w");
-	if (fp) {
-		SET_PERMS_STREAM(fp, 0, 0, 0644);
-		fclose(fp);
-	}
-	else {
+	// if the file already exists, do nothing
+	FILE *fp = fopen(fname, "wxe");
+	free(fname);
+	if (!fp) {
+		if (errno == EEXIST)
+			return;
 		fprintf(stderr, "Error: cannot create bandwidth file\n");
 		exit(1);
 	}
 
-	free(fname);
+	SET_PERMS_STREAM(fp, 0, 0, 0644);
+	fclose(fp);
 }
 
 
@@ -148,7 +142,7 @@ void network_set_run_file(pid_t pid) {
 		errExit("asprintf");
 
 	// create an empty file and set mod and ownership
-	FILE *fp = fopen(fname, "w");
+	FILE *fp = fopen(fname, "we");
 	if (fp) {
 		if (cfg.bridge0.configured)
 			fprintf(fp, "%s:%s\n", cfg.bridge0.dev, cfg.bridge0.devsandbox);
@@ -178,7 +172,7 @@ static void read_bandwidth_file(pid_t pid) {
 	if (asprintf(&fname, "%s/%d-bandwidth", RUN_FIREJAIL_BANDWIDTH_DIR, (int) pid) == -1)
 		errExit("asprintf");
 
-	FILE *fp = fopen(fname, "r");
+	FILE *fp = fopen(fname, "re");
 	if (fp) {
 		char buf[1024];
 		while (fgets(buf, 1024,fp)) {
@@ -214,7 +208,7 @@ static void write_bandwidth_file(pid_t pid) {
 	if (asprintf(&fname, "%s/%d-bandwidth", RUN_FIREJAIL_BANDWIDTH_DIR, (int) pid) == -1)
 		errExit("asprintf");
 
-	FILE *fp = fopen(fname, "w");
+	FILE *fp = fopen(fname, "we");
 	if (fp) {
 		IFBW *ptr = ifbw;
 		while (ptr) {
@@ -307,7 +301,7 @@ void bandwidth_pid(pid_t pid, const char *command, const char *dev, int down, in
 		char *fname;
 		if (asprintf(&fname, "%s/%d-netmap", RUN_FIREJAIL_NETWORK_DIR, (int) pid) == -1)
 			errExit("asprintf");
-		FILE *fp = fopen(fname, "r");
+		FILE *fp = fopen(fname, "re");
 		if (!fp) {
 			fprintf(stderr, "Error: cannot read network map file %s\n", fname);
 			exit(1);

src/firejail/caps.c
@@ -389,7 +389,7 @@ static uint64_t extract_caps(int pid) {
 		errExit("asprintf");
 
 	EUID_ROOT();	// grsecurity
-	FILE *fp = fopen(file, "r");
+	FILE *fp = fopen(file, "re");
 	EUID_USER();	// grsecurity
 	if (!fp)
 		goto errexit;

src/firejail/cgroup.c
@@ -26,7 +26,7 @@ void save_cgroup(void) {
 	if (cfg.cgroup == NULL)
 		return;
 
-	FILE *fp = fopen(RUN_CGROUP_CFG, "w");
+	FILE *fp = fopen(RUN_CGROUP_CFG, "wxe");
 	if (fp) {
 		fprintf(fp, "%s", cfg.cgroup);
 		fflush(0);
@@ -48,7 +48,7 @@ void load_cgroup(const char *fname) {
 	if (!fname)
 		return;
 
-	FILE *fp = fopen(fname, "r");
+	FILE *fp = fopen(fname, "re");
 	if (fp) {
 		char buf[MAXBUF];
 		if (fgets(buf, MAXBUF, fp)) {
@@ -91,19 +91,19 @@ void set_cgroup(const char *path) {
 		goto errout;
 
 	// tasks file exists
-	struct stat s;
-	if (stat(path, &s) == -1)
-		goto errout;
-
-	// task file belongs to the user running the sandbox
-	if (s.st_uid != getuid() && s.st_gid != getgid())
-		goto errout2;
-
-	// add the task to cgroup
-	/* coverity[toctou] */
-	FILE *fp = fopen(path,	"a");
+	FILE *fp = fopen(path, "ae");
 	if (!fp)
 		goto errout;
+	// task file belongs to the user running the sandbox
+	int fd = fileno(fp);
+	if (fd == -1)
+		errExit("fileno");
+	struct stat s;
+	if (fstat(fd, &s) == -1)
+		errExit("fstat");
+	if (s.st_uid != getuid() && s.st_gid != getgid())
+		goto errout2;
+	// add the task to cgroup
 	pid_t pid = getpid();
 	int rv = fprintf(fp, "%d\n", pid);
 	(void) rv;

src/firejail/checkcfg.c
@@ -59,7 +59,7 @@ int checkcfg(int val) {
 
 		// open configuration file
 		const char *fname = SYSCONFDIR "/firejail.config";
-		fp = fopen(fname, "r");
+		fp = fopen(fname, "re");
 		if (!fp) {
 #ifdef HAVE_GLOBALCFG
 			fprintf(stderr, "Error: Firejail configuration file %s not found\n", fname);

src/firejail/cpu.c
@@ -75,7 +75,7 @@ void save_cpu(void) {
 	if (cfg.cpus == 0)
 		return;
 
-	FILE *fp = fopen(RUN_CPU_CFG, "w");
+	FILE *fp = fopen(RUN_CPU_CFG, "wxe");
 	if (fp) {
 		fprintf(fp, "%x\n", cfg.cpus);
 		SET_PERMS_STREAM(fp, 0, 0, 0600);
@@ -91,7 +91,7 @@ void load_cpu(const char *fname) {
 	if (!fname)
 		return;
 
-	FILE *fp = fopen(fname, "r");
+	FILE *fp = fopen(fname, "re");
 	if (fp) {
 		unsigned tmp;
 		int rv = fscanf(fp, "%x", &tmp);
@@ -139,7 +139,7 @@ static void print_cpu(int pid) {
 	}
 
 	EUID_ROOT();	// grsecurity
-	FILE *fp = fopen(file, "r");
+	FILE *fp = fopen(file, "re");
 	EUID_USER();	// grsecurity
 	if (!fp) {
 		printf("  Error: cannot open %s\n", file);

src/firejail/dhcp.c
@@ -93,7 +93,7 @@ static pid_t dhcp_read_pidfile(const Dhclient *client) {
 	while (found == 0 && tries < 10) {
 		if (tries >= 1)
 			usleep(100000);
-		FILE *pidfile = fopen(client->pid_file, "r");
+		FILE *pidfile = fopen(client->pid_file, "re");
 		if (pidfile) {
 			long pid;
 			if (fscanf(pidfile, "%ld", &pid) == 1)

commit 518633eb42f2cf118c9411f9748328cb1fb19f7a

fix firejail startup race
sandboxes can race to create RUN_RO_FILE in shared memory

similiar to #1013

regression from 825ac9cdc38c4285584e69d6f29102b149914dfe

src/firejail/util.c
@@ -1030,8 +1030,10 @@ void create_empty_file_as_root(const char *fname, mode_t mode) {
 	if (stat(fname, &s)) {
 		if (arg_debug)
 			printf("Creating empty %s file\n", fname);
-
-		FILE *fp = fopen(fname, "wxe");
+		/* coverity[toctou] */
+		// don't fail if file already exists. This can be the case in a race
+		// condition, when two jails launch at the same time. Compare to #1013
+		FILE *fp = fopen(fname, "we");
 		if (!fp)
 			errExit("fopen");
 		SET_PERMS_STREAM(fp, 0, 0, mode);
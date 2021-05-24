518633eb42f2cf118c9411f9748328cb1fb19f7a
smitsohu <smitsohu@gmail.com>
2021-05-24 15:51:55 +0200

    fix firejail startup race
    sandboxes can race to create RUN_RO_FILE in shared memory
    
    similiar to #1013
    
    regression from 825ac9cdc38c4285584e69d6f29102b149914dfe

--- src/firejail/util.c
+++ src/firejail/util.c
@@ -1030,8 +1030,10 @@ void create_empty_file_as_root(const char *fname, mode_t mode) {
 	if (stat(fname, &s)) {
 		if (arg_debug)
 			printf("Creating empty %s file\n", fname);
-
-		FILE *fp = fopen(fname, "wxe");
+		/* coverity[toctou] */
+		// don't fail if file already exists. This can be the case in a race
+		// condition, when two jails launch at the same time. Compare to #1013
+		FILE *fp = fopen(fname, "we");
 		if (!fp)
 			errExit("fopen");
 		SET_PERMS_STREAM(fp, 0, 0, mode);

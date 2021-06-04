109c87b2c967caac2587fcd55f61836abf19de2b
Reiner Herrmann <reiner@reiner-h.de>
2021-06-04 22:34:33 +0200

    jailcheck: fix spelling errors

--- src/jailcheck/sysfiles.c
+++ src/jailcheck/sysfiles.c
@@ -34,7 +34,7 @@ void sysfiles_setup(const char *file) {
 	assert(file);
 
 	if (files_cnt >= MAX_TEST_FILES) {
-		fprintf(stderr, "Error: maximum number of system test files exceded\n");
+		fprintf(stderr, "Error: maximum number of system test files exceeded\n");
 		exit(1);
 	}
 

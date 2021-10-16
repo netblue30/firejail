f3001185bb91c4595ef1bde15e537c016edbd2a1
Kelvin M. Klann <kmk3.code@protonmail.com>
2021-10-16 02:41:38 -0300

    util.c: fix return type of get_group_id
    gr_gid is of type gid_t (not uid_t).  From grp.h(0p) of POSIX.1-2017:
    
    > DESCRIPTION
    >
    > The <grp.h> header shall declare the group structure, which shall
    > include the following members:
    >
    >    char   *gr_name The name of the group.
    >    gid_t   gr_gid  Numerical group ID.
    >    char  **gr_mem  Pointer to a null-terminated array of character
    >                    pointers to member names.
    >
    > The <grp.h> header shall define the gid_t and size_t types as
    > described in <sys/types.h>.
    
    Note: The callers already store the result in gid_t variables.
    
    First caused by commit dc3564b18 ("fixes", 2016-03-09).

--- src/firejail/util.c
+++ src/firejail/util.c
@@ -959,7 +959,7 @@ uid_t pid_get_uid(pid_t pid) {
 }
 
 
-uid_t get_group_id(const char *group) {
+gid_t get_group_id(const char *group) {
 	gid_t gid = 0;
 	struct group *g = getgrnam(group);
 	if (g)

021a4c4a73f8bc60b51f1524e55a06f3af7df457
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2021-10-30 16:34:48 +0200

    prep for meson

--- mkman.sh
+++ mkman.sh
@@ -5,8 +5,8 @@
 
 set -e
 
-sed "s/VERSION/$1/g" $2 > $3
+sed "s/@VERSION@/$1/g" $2 > $3
 MONTH=`LC_ALL=C date -u --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%b`
-sed -i "s/MONTH/$MONTH/g" $3
+sed -i "s/@MONTH@/$MONTH/g" $3
 YEAR=`LC_ALL=C date -u --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y`
-sed -i "s/YEAR/$YEAR/g" $3
+sed -i "s/@YEAR@/$YEAR/g" $3

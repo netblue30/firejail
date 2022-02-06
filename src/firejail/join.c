netblue30/firejail

commit c764520b5aa343c00c3a73633511df039645973c

keep-fd option (#4845)

src/firejail/join.c
@@ -569,11 +569,6 @@ void join(pid_t pid, int argc, char **argv, int index) {
 			dbus_set_system_bus_env();
 #endif
 
-		// set nice and rlimits
-		if (arg_nice)
-			set_nice(cfg.nice);
-		set_rlimits();
-
 		start_application(0, shfd, NULL);
 
 		__builtin_unreachable();

contrib/vim/syntax/firejail.vim
@@ -51,7 +51,7 @@ syn match fjVar /\v\$\{(CFG|DESKTOP|DOCUMENTS|DOWNLOADS|HOME|MUSIC|PATH|PICTURES
 " Generate list with: { rg -o 'strn?cmp\(ptr, "([^"]+) "' -r '$1' src/firejail/profile.c; echo private-lib; } | grep -vEx '(include|ignore|caps\.drop|caps\.keep|protocol|seccomp|seccomp\.drop|seccomp\.keep|env|rmenv|net|ip)' | sort -u | tr $'\n' '|' # private-lib is special-cased in the code and doesn't match the regex; grep-ed patterns are handled later with 'syn match nextgroup=' directives (except for include which is special-cased as a fjCommandNoCond keyword)
 syn match fjCommand /\v(bind|blacklist|blacklist-nolog|cgroup|cpu|defaultgw|dns|hostname|hosts-file|ip6|iprange|join-or-start|mac|mkdir|mkfile|mtu|name|netfilter|netfilter6|netmask|nice|noblacklist|noexec|nowhitelist|overlay-named|private|private-bin|private-cwd|private-etc|private-home|private-lib|private-opt|private-srv|read-only|read-write|rlimit-as|rlimit-cpu|rlimit-fsize|rlimit-nofile|rlimit-nproc|rlimit-sigpending|timeout|tmpfs|veth-name|whitelist|xephyr-screen) / skipwhite contained
 " Generate list with: rg -o 'strn?cmp\(ptr, "([^ "]*[^ ])"' -r '$1' src/firejail/profile.c | grep -vEx '(include|rlimit|quiet)' | sed -e 's/\./\\./' | sort -u | tr $'\n' '|' # include/rlimit are false positives, quiet is special-cased below
-syn match fjCommand /\v(allow-debuggers|allusers|apparmor|caps|deterministic-exit-code|deterministic-shutdown|disable-mnt|ipc-namespace|keep-config-pulse|keep-dev-shm|keep-var-tmp|machine-id|memory-deny-write-execute|netfilter|no3d|noautopulse|nodbus|nodvd|nogroups|noinput|nonewprivs|noprinters|noroot|nosound|notv|nou2f|novideo|overlay|overlay-tmpfs|private|private-cache|private-cwd|private-dev|private-lib|private-tmp|seccomp|seccomp\.32|seccomp\.block-secondary|tracelog|writable-etc|writable-run-user|writable-var|writable-var-log|x11)$/ contained
+syn match fjCommand /\v(allow-debuggers|allusers|apparmor|caps|deterministic-exit-code|deterministic-shutdown|disable-mnt|ipc-namespace|keep-config-pulse|keep-dev-shm|keep-fd|keep-var-tmp|machine-id|memory-deny-write-execute|netfilter|no3d|noautopulse|nodbus|nodvd|nogroups|noinput|nonewprivs|noprinters|noroot|nosound|notv|nou2f|novideo|overlay|overlay-tmpfs|private|private-cache|private-cwd|private-dev|private-lib|private-tmp|seccomp|seccomp\.32|seccomp\.block-secondary|tracelog|writable-etc|writable-run-user|writable-var|writable-var-log|x11)$/ contained
 syn match fjCommand /ignore / nextgroup=fjCommand,fjCommandNoCond skipwhite contained
 syn match fjCommand /caps\.drop / nextgroup=fjCapability,fjAll skipwhite contained
 syn match fjCommand /caps\.keep / nextgroup=fjCapability skipwhite contained

src/firejail/dbus.c
@@ -298,10 +298,10 @@ void dbus_proxy_start(void) {
 		errExit("fork");
 	if (dbus_proxy_pid == 0) {
 		// close open files
-		int keep_list[2];
-		keep_list[0] = status_pipe[1];
-		keep_list[1] = args_pipe[0];
-		close_all(keep_list, ARRAY_SIZE(keep_list));
+		int keep[2];
+		keep[0] = status_pipe[1];
+		keep[1] = args_pipe[0];
+		close_all(keep, ARRAY_SIZE(keep));
 
 		if (arg_dbus_log_file != NULL) {
 			int output_fd = creat(arg_dbus_log_file, 0666);

src/firejail/firejail.h
@@ -161,6 +161,7 @@ typedef struct config_t {
 
 #define MAX_PROFILE_IGNORE 32
 	char *profile_ignore[MAX_PROFILE_IGNORE];
+	char *keep_fd;		// inherit file descriptors to sandbox
 	char *chrootdir;	// chroot directory
 	char *home_private;	// private home directory
 	char *home_private_keep;	// keep list for private home directory
@@ -352,6 +353,7 @@ extern int arg_nou2f;	// --nou2f
 extern int arg_noinput;	// --noinput
 extern int arg_deterministic_exit_code;	// always exit with first child's exit status
 extern int arg_deterministic_shutdown;	// shut down the sandbox if first child dies
+extern int arg_keep_fd_all;	// inherit all file descriptors to sandbox
 
 typedef enum {
 	DBUS_POLICY_ALLOW,	// Allow unrestricted access to the bus

src/firejail/main.c
@@ -149,6 +149,7 @@ int arg_nou2f = 0; // --nou2f
 int arg_noinput = 0; // --noinput
 int arg_deterministic_exit_code = 0;	// always exit with first child's exit status
 int arg_deterministic_shutdown = 0; 	// shut down the sandbox if first child dies
+int arg_keep_fd_all = 0;		// inherit all file descriptors to sandbox
 DbusPolicy arg_dbus_user = DBUS_POLICY_ALLOW;	// --dbus-user
 DbusPolicy arg_dbus_system = DBUS_POLICY_ALLOW;	// --dbus-system
 const char *arg_dbus_log_file = NULL;
@@ -1861,6 +1862,14 @@ int main(int argc, char **argv, char **envp) {
 				exit(1);
 			}
 			profile_add_ignore(argv[i] + 9);
+		}
+		else if (strncmp(argv[i], "--keep-fd=", 10) == 0) {
+			if (strcmp(argv[i] + 10, "all") == 0)
+				arg_keep_fd_all = 1;
+			else {
+				const char *add = argv[i] + 10;
+				profile_list_augment(&cfg.keep_fd, add);
+			}
 		}
 #ifdef HAVE_CHROOT
 		else if (strncmp(argv[i], "--chroot=", 9) == 0) {

src/firejail/profile.c
@@ -290,6 +290,15 @@ int profile_check_line(char *ptr, int lineno, const char *fname) {
 		return 0;
 	}
 
+	if (strncmp(ptr, "keep-fd ", 8) == 0) {
+		if (strcmp(ptr + 8, "all") == 0)
+			arg_keep_fd_all = 1;
+		else {
+			const char *add = ptr + 8;
+			profile_list_augment(&cfg.keep_fd, add);
+		}
+		return 0;
+	}
 	if (strncmp(ptr, "xephyr-screen ", 14) == 0) {
 #ifdef HAVE_X11
 		if (checkcfg(CFG_X11)) {

src/firejail/sandbox.c
@@ -399,9 +399,25 @@ static int monitor_application(pid_t app_pid) {
 	return arg_deterministic_exit_code ? app_status : status;
 }
 
+
 static void print_time(void) {
 	float delta = timetrace_end();
 	fmessage("Child process initialized in %.02f ms\n", delta);
+}
+
+
+int *build_keep_fd_array(size_t *sz) {
+	if (!cfg.keep_fd) {
+		*sz = 0;
+		return NULL;
+	}
+
+	int *rv = str_to_int_array(cfg.keep_fd, sz);
+	if (!rv) {
+		fprintf(stderr, "Error: invalid keep-fd option\n");
+		exit(1);
+	}
+	return rv;
 }
 
 
@@ -461,10 +477,27 @@ static int ok_to_run(const char *program) {
 	return 0;
 }
 
+
 void start_application(int no_sandbox, int fd, char *set_sandbox_status) {
-	// set environment
-	if (no_sandbox == 0)
+	if (no_sandbox == 0) {
+		// don't leak open file descriptors
+		if (!arg_keep_fd_all) {
+			size_t sz;
+			int *keep = build_keep_fd_array(&sz);
+			close_all(keep, sz);
+			if (keep)
+				free(keep);
+		}
+
+		// set nice and rlimits
+		if (arg_nice)
+			set_nice(cfg.nice);
+		set_rlimits();
+
 		env_defaults();
+	}
+
+	// set environment
 	env_apply_all();
 
 	// restore original umask
@@ -1252,12 +1285,6 @@ int sandbox(void* sandbox_arg) {
 #ifdef HAVE_APPARMOR
 		set_apparmor();
 #endif
-
-		// set nice and rlimits
-		if (arg_nice)
-			set_nice(cfg.nice);
-		set_rlimits();
-
 		start_application(0, -1, set_sandbox_status);
 	}
 

src/firejail/usage.c
@@ -119,6 +119,7 @@ static char *usage_str =
 	"    --join-or-start=name|pid - join the sandbox or start a new one.\n"
 	"    --keep-config-pulse - disable automatic ~/.config/pulse init.\n"
 	"    --keep-dev-shm - /dev/shm directory is untouched (even with --private-dev).\n"
+	"    --keep-fd - inherit open file descriptors to sandbox.\n"
 	"    --keep-var-tmp - /var/tmp directory is untouched.\n"
 	"    --list - list all sandboxes.\n"
 #ifdef HAVE_FILE_TRANSFER

commit 70f2cd167f9080563224b035d04bc568b16c4b6c

fix joining of sandboxes without shell
regressed in c764520b5aa343c00c3a73633511df039645973c

src/firejail/join.c
@@ -315,6 +315,11 @@ static int open_shell(void) {
 		fprintf(stderr, "Error: cannot open shell %s\n", cfg.shell);
 		exit(1);
 	}
+
+	// pass file descriptor through to the final fexecve
+	if (asprintf(&cfg.keep_fd, "%s,%d", cfg.keep_fd ? cfg.keep_fd : "", fd) == -1)
+		errExit("asprintf");
+
 	return fd;
 }
 
b900bdc87463d79568aef46cb7e3b373fbff84b1
Kelvin M. Klann <kmk3.code@protonmail.com>
2022-08-29 11:04:18 -0300

    Revert "compile fix"
    This reverts commit 836ffe37ff891886f15243eacc70963368d57a3f.
    
    Part of reverting commits with Landlock-related changes.

--- src/firejail/landlock.c
+++ src/firejail/landlock.c
@@ -1,24 +1,3 @@
-/*
- * Copyright (C) 2014-2022 Firejail Authors
- *
- * This file is part of firejail project
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
-
-#ifdef HAVE_LANDLOCK
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stddef.h>
@@ -98,4 +77,3 @@ int add_execute_rule_by_path(int rset_fd,char *allowed_path) {
 	close(allowed_fd);
 	return result;
 }
-#endif

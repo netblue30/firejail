d7826c2384b6aa655d92b0570bebdc9cef8cb393
a1346054 <36859588+a1346054@users.noreply.github.com>
2022-09-13 19:47:01 +0000

    tests: use an array

--- test/private-lib/private-lib.sh
+++ test/private-lib/private-lib.sh
@@ -7,10 +7,9 @@ export MALLOC_CHECK_=3g
 export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
 export LC_ALL=C
 
-LIST="gnome-logs gnome-system-log gnome-nettool pavucontrol dig evince whois galculator gnome-calculator gedit leafpad mousepad pluma transmission-gtk xcalc atril gpicview eom eog"
+apps=(gnome-logs gnome-system-log gnome-nettool pavucontrol dig evince whois galculator gnome-calculator gedit leafpad mousepad pluma transmission-gtk xcalc atril gpicview eom eog)
 
-
-for app in $LIST; do
+for app in "${apps[@]}"; do
 	if command -v "$app"
 	then
 		echo "TESTING: private-lib $app"

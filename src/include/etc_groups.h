de82aef79a151708fbee113ccc523dbde5fc1113
glitsj16 <glitsj16@users.noreply.github.com>
2023-02-04 17:19:44 +0000

    etc_groups.h: internally sort groups alphabetically

--- src/include/etc_groups.h
+++ src/include/etc_groups.h
@@ -27,6 +27,7 @@
 static char *etc_list[ETC_MAX + 1] = { // plus 1 for ending NULL pointer
 	"alternatives",
 	"fonts",
+	"group",
 	"ld.so.cache",
 	"ld.so.conf",
 	"ld.so.conf.d",
@@ -38,7 +39,6 @@ static char *etc_list[ETC_MAX + 1] = { // plus 1 for ending NULL pointer
 	"login.defs", // firejail reading UID/GID MIN and MAX at startup
 	"nsswitch.conf",
 	"passwd",
-	"group",
 	NULL
 };
 
@@ -55,8 +55,8 @@ static char *etc_group_sound[] = {
 static char*etc_group_network[] = {
 	"hostname",
 	"hosts",
-	"resolv.conf",
 	"protocols",
+	"resolv.conf",
 	NULL
 };
 
@@ -72,24 +72,24 @@ static char *etc_group_tls_ca[] = {
 
 // @x11
 static char *etc_group_x11[] = {
-	"xdg",
-	"drirc",
 	"dconf",
+	"drirc",
 	"gtk-2.0",
 	"gtk-3.0",
 	"kde4rc",
 	"kde5rc",
-	"pango", // text rendering/internationalization
 	"nvidia",
+	"pango", // text rendering/internationalization
 	"X11",
+	"xdg",
 	NULL
 };
 
 // @games
 static char *etc_group_games[] = {
+	"openal", // 3D sound
 	"timidity", // MIDI
 	"timidity.cfg",
-	"openal", // 3D sound
 	NULL
 };
 

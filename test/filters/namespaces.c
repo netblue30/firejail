def30f6d1e6cbde702dfcf2150d65bdddc9ac75e
Kelvin M. Klann <kmk3.code@protonmail.com>
2023-02-19 17:22:00 -0300

    test/filters/namespaces.c: use tabs for indentation
    This appears to be the only C file in the repository that uses spaces
    for indentation.
    
    Commands used to check for the above:
    
        $ git grep '^        ' -- '*.c' '*.h'
    
    Commands used to search and replace:
    
        $ f=test/filters/namespaces.c; printf '%s\n' \
            "$(sed 's/    /\t/g' "$f")" >"$f"
    
    Note: The mmap call was aligned manually.
    
    Added on commit 5116c1ced ("testing", 2022-12-24).

--- test/filters/namespaces.c
+++ test/filters/namespaces.c
@@ -15,82 +15,82 @@
 #define STACK_SIZE 1024 * 1024
 
 static int usage() {
-    fprintf(stderr, "Usage: namespaces <system call>[clone,unshare] <list of namespaces>[cgroup,ipc,mnt,net,pid,time,user,uts]\n");
-    exit(1);
+	fprintf(stderr, "Usage: namespaces <system call>[clone,unshare] <list of namespaces>[cgroup,ipc,mnt,net,pid,time,user,uts]\n");
+	exit(1);
 }
 
 static void die(const char *msg) {
-    fprintf(stderr, "Error: %s: %s\n", msg, strerror(errno));
-    exit(1);
+	fprintf(stderr, "Error: %s: %s\n", msg, strerror(errno));
+	exit(1);
 }
 
 static int ns_flags(const char *list) {
-    int flags = 0;
+	int flags = 0;
 
-    char *dup = strdup(list);
-    if (!dup)
-        die("cannot allocate memory");
+	char *dup = strdup(list);
+	if (!dup)
+		die("cannot allocate memory");
 
-    char *token = strtok(dup, ",");
-    while (token) {
-        if (strcmp(token, "cgroup") == 0)
-            flags |= CLONE_NEWCGROUP;
-        else if (strcmp(token, "ipc") == 0)
-            flags |= CLONE_NEWIPC;
-        else if (strcmp(token, "net") == 0)
-            flags |= CLONE_NEWNET;
-        else if (strcmp(token, "mnt") == 0)
-            flags |= CLONE_NEWNS;
-        else if (strcmp(token, "pid") == 0)
-            flags |= CLONE_NEWPID;
-        else if (strcmp(token, "time") == 0)
-            flags |= CLONE_NEWTIME;
-        else if (strcmp(token, "user") == 0)
-            flags |= CLONE_NEWUSER;
-        else if (strcmp(token, "uts") == 0)
-            flags |= CLONE_NEWUTS;
-        else
-            usage();
+	char *token = strtok(dup, ",");
+	while (token) {
+		if (strcmp(token, "cgroup") == 0)
+			flags |= CLONE_NEWCGROUP;
+		else if (strcmp(token, "ipc") == 0)
+			flags |= CLONE_NEWIPC;
+		else if (strcmp(token, "net") == 0)
+			flags |= CLONE_NEWNET;
+		else if (strcmp(token, "mnt") == 0)
+			flags |= CLONE_NEWNS;
+		else if (strcmp(token, "pid") == 0)
+			flags |= CLONE_NEWPID;
+		else if (strcmp(token, "time") == 0)
+			flags |= CLONE_NEWTIME;
+		else if (strcmp(token, "user") == 0)
+			flags |= CLONE_NEWUSER;
+		else if (strcmp(token, "uts") == 0)
+			flags |= CLONE_NEWUTS;
+		else
+			usage();
 
-        token = strtok(NULL, ",");
-    }
+		token = strtok(NULL, ",");
+	}
 
-    free(dup);
-    return flags;
+	free(dup);
+	return flags;
 }
 
 static int child(void *arg) {
-    (void) arg;
+	(void) arg;
 
-    fprintf(stderr, "clone successful\n");
-    return 0;
+	fprintf(stderr, "clone successful\n");
+	return 0;
 }
 
 int main (int argc, char **argv) {
-    if (argc != 3)
-        usage();
+	if (argc != 3)
+		usage();
 
-    int flags = ns_flags(argv[2]);
-    if (getuid() != 0)
-        flags |= CLONE_NEWUSER;
+	int flags = ns_flags(argv[2]);
+	if (getuid() != 0)
+		flags |= CLONE_NEWUSER;
 
-    if (strcmp(argv[1], "clone") == 0) {
-        void *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
-                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        if (stack == MAP_FAILED)
-            die("mmap");
+	if (strcmp(argv[1], "clone") == 0) {
+		void *stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE,
+		                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		if (stack == MAP_FAILED)
+			die("mmap");
 
-        if (clone(child, stack + STACK_SIZE, flags | SIGCHLD, NULL) < 0)
-            die("clone");
-    }
-    else if (strcmp(argv[1], "unshare") == 0) {
-        if (unshare(flags))
-            die("unshare");
+		if (clone(child, stack + STACK_SIZE, flags | SIGCHLD, NULL) < 0)
+			die("clone");
+	}
+	else if (strcmp(argv[1], "unshare") == 0) {
+		if (unshare(flags))
+			die("unshare");
 
-        fprintf(stderr, "unshare successful\n");
-    }
-    else
-        usage();
+		fprintf(stderr, "unshare successful\n");
+	}
+	else
+		usage();
 
-    return 0;
+	return 0;
 }

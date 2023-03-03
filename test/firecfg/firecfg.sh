774fadedeebf92fecc90b10efcef682c547f573b
netblue30 <netblue30@protonmail.com>
2023-03-03 17:16:45 -0500

    testing firecfg

--- test/firecfg/firecfg.sh
+++ test/firecfg/firecfg.sh
@@ -7,17 +7,6 @@ export MALLOC_CHECK_=3
 export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
 export LC_ALL=C
 
-sudo cat /sys/kernel/security/apparmor/profiles
 sudo firecfg
-file /usr/local/bin/ping
-ls -l /usr/local/bin
-sudo cat /sys/kernel/security/apparmor/profiles
-
-#if [[ -f /sys/kernel/security/apparmor/profiles ]]; then
-#
-#else
-#	echo "TESTING SKIP: no apparmor support in Linux kernel (test/filters/apparmor.exp)"
-#fi
-#
-#echo "TESTING: firecfg (test/firecfg/firecfg.exp)"
-#./firecfg.exp.exp
+echo "TESTING: firecfg (test/firecfg/firecfg.exp)"
+./firecfg.exp

e06c3e99d8d42d3422bf8ebf50fd500da1cf4ccf
Kelvin M. Klann <kmk3.code@protonmail.com>
2023-06-17 15:09:35 -0300

    common.h: use __func__ instead of __FUNCTION__
    For increased portability.
    
    The former is in C99, the latter is from gcc.

--- src/include/common.h
+++ src/include/common.h
@@ -32,12 +32,16 @@
 #include <ctype.h>
 #include <assert.h>
 
+#if !defined(__func__) && defined(__FUNCTION__)
+#define __func__ __FUNCTION__
+#endif
+
 // dbus proxy path used by firejail and firemon
 #define XDG_DBUS_PROXY_PATH "/usr/bin/xdg-dbus-proxy"
 
 #define errExit(msg) do { \
 	char msgout[500]; \
-	snprintf(msgout, 500, "Error %s: %s:%d %s", msg, __FILE__, __LINE__, __FUNCTION__); \
+	snprintf(msgout, 500, "Error %s: %s:%d %s", msg, __FILE__, __LINE__, __func__); \
 	perror(msgout); \
 	exit(1); \
 } while (0)

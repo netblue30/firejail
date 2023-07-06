e665769f5225062565b3a75677ce43eb37b988c5
Kelvin M. Klann <kmk3.code@protonmail.com>
2023-07-05 23:56:06 -0300

    build: automatically generate header dependencies
    Instead of manually specifying which source files depend on which
    headers, use compiler flags to automatically generate depfiles (.d),
    which declare the correct header (make) dependencies for each source
    file (.c).
    
    Use `-MMD` (which ignores system headers) to generate the dependencies
    and `-MP` to prevent make from complaining when a header file is removed
    while it is listed as a dependency in a depfile.
    
    If depfiles exist, just include them.  If not, make each object file
    (.o) unconditionally depend on all header files in its source directory
    and in src/include, to ensure that rebuilds are done when needed.  The
    latter case applies to the first build after `make clean` (which would
    build everything anyway) and when the compiler does not support
    generating depfiles.
    
    Note that both gcc and clang have supported these options for a long
    time.
    
    Misc: This depends on the changes from commit 5b1bd33c7 ("build: use
    full paths on compile/link targets", 2023-07-02) / PR #6158 to avoid
    issues with make dependency tracking.

--- src/so.mk
+++ src/so.mk
@@ -3,16 +3,22 @@
 # Note: $(ROOT)/config.mk must be included before this file.
 #
 # The includer should probably define SO and TARGET and may also want to define
-# EXTRA_HDRS and EXTRA_OBJS and extend CLEANFILES.
+# EXTRA_OBJS and extend CLEANFILES.
 
-HDRS := $(sort $(wildcard $(MOD_DIR)/*.h))
+HDRS :=
 SRCS := $(sort $(wildcard $(MOD_DIR)/*.c))
 OBJS := $(SRCS:.c=.o)
+DEPS := $(sort $(wildcard $(OBJS:.o=.d)))
+
+ifeq ($(DEPS),)
+HDRS := $(sort $(wildcard $(MOD_DIR)/*.h $(ROOT)/src/include/*.h))
+endif
 
 .PHONY: all
 all: $(TARGET)
+-include $(DEPS)
 
-%.o : %.c $(HDRS) $(EXTRA_HDRS) $(ROOT)/config.mk
+%.o : %.c $(HDRS) $(ROOT)/config.mk
 	$(CC) $(SO_CFLAGS) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
 
 $(SO): $(OBJS) $(EXTRA_OBJS) $(ROOT)/config.mk

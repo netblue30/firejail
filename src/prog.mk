54057e15f6160d2d8691b5f193063fd95495a37b
Kelvin M. Klann <kmk3.code@protonmail.com>
2023-07-11 03:30:05 -0300

    build: move EXTRA vars directly into targets

--- src/prog.mk
+++ src/prog.mk
@@ -5,18 +5,18 @@
 # The includer should probably define PROG and TARGET and may also want to
 # define EXTRA_HDRS and EXTRA_OBJS and extend CLEANFILES.
 
-HDRS := $(sort $(wildcard $(MOD_DIR)/*.h)) $(EXTRA_HDRS)
+HDRS := $(sort $(wildcard $(MOD_DIR)/*.h))
 SRCS := $(sort $(wildcard $(MOD_DIR)/*.c))
-OBJS := $(SRCS:.c=.o) $(EXTRA_OBJS)
+OBJS := $(SRCS:.c=.o)
 
 .PHONY: all
 all: $(TARGET)
 
-%.o : %.c $(HDRS) $(ROOT)/config.mk
+%.o : %.c $(HDRS) $(EXTRA_HDRS) $(ROOT)/config.mk
 	$(CC) $(PROG_CFLAGS) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
 
-$(PROG): $(OBJS) $(ROOT)/config.mk
-	$(CC) $(PROG_LDFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(LIBS)
+$(PROG): $(OBJS) $(EXTRA_OBJS) $(ROOT)/config.mk
+	$(CC) $(PROG_LDFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(EXTRA_OBJS) $(LIBS)
 
 .PHONY: clean
 clean:; rm -fr $(PROG) $(CLEANFILES)
--- src/so.mk
+++ src/so.mk
@@ -5,18 +5,18 @@
 # The includer should probably define SO and TARGET and may also want to define
 # EXTRA_HDRS and EXTRA_OBJS and extend CLEANFILES.
 
-HDRS := $(sort $(wildcard $(MOD_DIR)/*.h)) $(EXTRA_HDRS)
+HDRS := $(sort $(wildcard $(MOD_DIR)/*.h))
 SRCS := $(sort $(wildcard $(MOD_DIR)/*.c))
-OBJS := $(SRCS:.c=.o) $(EXTRA_OBJS)
+OBJS := $(SRCS:.c=.o)
 
 .PHONY: all
 all: $(TARGET)
 
-%.o : %.c $(HDRS) $(ROOT)/config.mk
+%.o : %.c $(HDRS) $(EXTRA_HDRS) $(ROOT)/config.mk
 	$(CC) $(SO_CFLAGS) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
 
-$(SO): $(OBJS) $(ROOT)/config.mk
-	$(CC) $(SO_LDFLAGS) -shared $(LDFLAGS) -o $@ $(OBJS) -ldl
+$(SO): $(OBJS) $(EXTRA_OBJS) $(ROOT)/config.mk
+	$(CC) $(SO_LDFLAGS) -shared $(LDFLAGS) -o $@ $(OBJS) $(EXTRA_OBJS) -ldl
 
 .PHONY: clean
 clean:; rm -fr $(SO) $(CLEANFILES)

netblue30/firejail

commit d2802ce6068e5e9296e7b9a06a9c82e18ca23972

fnettrace cleanup

src/fnettrace/hostnames.c
@@ -50,12 +50,13 @@ char *retrieve_hostname(uint32_t ip) {
 				ptr = buf + 22;
 				if (*ptr == ' ' && *(ptr + 3) == ',' &&  *(ptr + 4) == ' ') {
 					rv = ptr + 5;
-					rv = radix_add(ip, 0xffffffff, rv);
+					if (strcmp(rv, "United States") == 0)
+						rv = "US";
 				}
 			}
 		}
 		pclose(fp);
-		return rv;
+		return strdup(rv);
 	}
 	else
 		geoip_not_found = 1;

src/fnettrace/main.c
@@ -33,13 +33,16 @@ typedef struct hnode_t {
 	struct hnode_t *hnext;	// used for hash table and unused linked list
 	struct hnode_t *dnext;	// used to display streams on the screen
 	uint32_t ip_src;
+	RNode *rnode;	// radix tree entry
+
+	// stats
 	uint32_t  bytes;	// number of bytes received in the last display interval
 	uint16_t port_src;
 	uint8_t protocol;
+
 	// the firewall is build based on source address, and in the linked list
-	// we have elements with the same address but different ports
+	// we could have elements with the same address but different ports
 	uint8_t ip_instance;
-	char *hostname;
 	int ttl;
 } HNode;
 
@@ -89,6 +92,8 @@ static void hnode_add(uint32_t ip_src, uint8_t protocol, uint16_t port_src, uint
 			ip_instance++;
 			if (ptr->port_src == port_src && ptr->protocol == protocol) {
 				ptr->bytes += bytes;
+				assert(ptr->rnode);
+				ptr->rnode->pkts++;
 				return;
 			}
 		}
@@ -100,7 +105,6 @@ static void hnode_add(uint32_t ip_src, uint8_t protocol, uint16_t port_src, uint
 #endif
 	HNode *hnew = hmalloc();
 	assert(hnew);
-	hnew->hostname = NULL;
 	hnew->ip_src = ip_src;
 	hnew->port_src = port_src;
 	hnew->protocol = protocol;
@@ -125,6 +129,11 @@ static void hnode_add(uint32_t ip_src, uint8_t protocol, uint16_t port_src, uint
 			ptr = ptr->dnext;
 		ptr->dnext = hnew;
 	}
+
+	hnew->rnode = radix_longest_prefix_match(hnew->ip_src);
+	if (!hnew->rnode)
+		hnew->rnode = radix_add(hnew->ip_src, 0xffffffff, NULL);
+	hnew->rnode->pkts++;
 
 	if (arg_netfilter)
 		logprintf(" %d.%d.%d.%d ", PRINT_IP(hnew->ip_src));
@@ -242,15 +251,15 @@ static PortType ports[] = {
 	{110, "(POP3)"},
 	{113, "(IRC)"},
 	{123, "(NTP)"},
-	{161, "(SNP)"},
-	{162, "(SNP)"},
+	{161, "(SNMP)"},
+	{162, "(SNMP)"},
 	{194, "(IRC)"},
 	{0, NULL},
 };
 
 
 static inline const char *common_port(uint16_t port) {
-	if (port >= 6660 && port <= 9150) {
+	if (port >= 6660 && port <= 10162) {
 		if (port >= 6660 && port <= 6669)
 			return "(IRC)";
 		else if (port == 6679)
@@ -269,6 +278,10 @@ static inline const char *common_port(uint16_t port) {
 			return "(Tor)";
 		else if (port == 9150)
 			return "(Tor)";
+		else if (port == 10161)
+			return "(secure SNMP)";
+		else if (port == 10162)
+			return "(secure SNMP)";
 		return NULL;
 	}
 
@@ -317,7 +330,8 @@ static void hnode_print(unsigned bw) {
 		sprintf(stats, "%u MB/s ", bw / (1024 * 1024 * DISPLAY_INTERVAL));
 	else
 		sprintf(stats, "%u KB/s ", bw / (1024 * DISPLAY_INTERVAL));
-	int len = snprintf(line, LINE_MAX, "%32s geoip %d, IP database %d\n", stats, geoip_calls, radix_nodes);
+//	int len = snprintf(line, LINE_MAX, "%32s geoip %d, IP database %d\n", stats, geoip_calls, radix_nodes);
+	int len = snprintf(line, LINE_MAX, "%32s address:port (protocol) host (packets)\n", stats);
 	adjust_line(line, len, cols);
 	printf("%s", line);
 
@@ -336,12 +350,11 @@ static void hnode_print(unsigned bw) {
 			else
 				snprintf(bytes, 11, "%u B/s ", (unsigned) (ptr->bytes / DISPLAY_INTERVAL));
 
-			if (!ptr->hostname)
-				ptr->hostname = radix_longest_prefix_match(ptr->ip_src);
-			if (!ptr->hostname)
-				ptr->hostname = retrieve_hostname(ptr->ip_src);
-			if (!ptr->hostname)
-				ptr->hostname = " ";
+			if (!ptr->rnode->name)
+				ptr->rnode->name = retrieve_hostname(ptr->ip_src);
+			if (!ptr->rnode->name)
+				ptr->rnode->name = " ";
+			assert(ptr->rnode->name);
 
 			unsigned bwunit = bw / DISPLAY_BW_UNITS;
 			char *bwline;
@@ -376,11 +389,16 @@ static void hnode_print(unsigned bw) {
 				protocol = "";
 			if (ptr->port_src == 0)
 				len = snprintf(line, LINE_MAX, "%10s %s %d.%d.%d.%d (ICMP) %s\n",
-					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->hostname);
+					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->rnode->name);
+			else if (ptr->rnode->pkts > 1000000)
+				len = snprintf(line, LINE_MAX, "%10s %s %d.%d.%d.%d:%u%s %s (%.01fM)\n",
+					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->port_src, protocol, ptr->rnode->name, ((double) ptr->rnode->pkts) / 1000000);
+			else if (ptr->rnode->pkts > 1000)
+				len = snprintf(line, LINE_MAX, "%10s %s %d.%d.%d.%d:%u%s %s (%.01fK)\n",
+					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->port_src, protocol, ptr->rnode->name, ((double) ptr->rnode->pkts) / 1000);
 			else
-				len = snprintf(line, LINE_MAX, "%10s %s %d.%d.%d.%d:%u%s %s\n",
-					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->port_src, protocol, ptr->hostname);
-
+				len = snprintf(line, LINE_MAX, "%10s %s %d.%d.%d.%d:%u%s %s (%u)\n",
+					       bytes, bwline, PRINT_IP(ptr->ip_src), ptr->port_src, protocol, ptr->rnode->name, ptr->rnode->pkts);
 			adjust_line(line, len, cols);
 			printf("%s", line);
 

src/fnettrace/radix.c
@@ -25,12 +25,6 @@
 #include "radix.h"
 #include "fnettrace.h"
 
-typedef struct rnode_t {
-	struct rnode_t *zero;
-	struct rnode_t *one;
-	char *name;
-} RNode;
-
 RNode *head = 0;
 int radix_nodes = 0;
 
@@ -100,8 +94,7 @@ static inline RNode *addZero(RNode *ptr, char *name) {
 
 
 // add to radix tree
-char *radix_add(uint32_t ip, uint32_t mask, char *name) {
-	assert(name);
+RNode *radix_add(uint32_t ip, uint32_t mask, char *name) {
 	uint32_t m = 0x80000000;
 	uint32_t lastm = 0;
 	if (head == 0) {
@@ -124,17 +117,17 @@ char *radix_add(uint32_t ip, uint32_t mask, char *name) {
 			ptr = addZero(ptr, (valid)? name: NULL);
 	}
 	assert(ptr);
-	if (!ptr->name) {
+	if (name && !ptr->name) {
 		ptr->name = duplicate_name(name);
 		if (!ptr->name)
 			errExit("duplicate_name");
 	}
 
-	return ptr->name;
+	return ptr;
 }
 
 // find last match
-char *radix_longest_prefix_match(uint32_t ip) {
+RNode *radix_longest_prefix_match(uint32_t ip) {
 	if (!head)
 		return NULL;
 
@@ -154,7 +147,7 @@ char *radix_longest_prefix_match(uint32_t ip) {
 			rv = ptr;
 	}
 
-	return (rv)? rv->name: NULL;
+	return rv;
 }
 
 static uint32_t sum;

src/fnettrace/radix.h
@@ -20,9 +20,16 @@
 #ifndef RADIX_H
 #define RADIX_H
 
+typedef struct rnode_t {
+	struct rnode_t *zero;
+	struct rnode_t *one;
+	char *name;
+	uint32_t pkts;
+} RNode;
+
 extern int radix_nodes;
-char *radix_longest_prefix_match(uint32_t ip);
-char *radix_add(uint32_t ip, uint32_t mask, char *name);
+RNode *radix_longest_prefix_match(uint32_t ip);
+RNode*radix_add(uint32_t ip, uint32_t mask, char *name);
 void radix_print(void);
 void radix_squash(void);
 

commit 142f55107064d4cbd4e70ae625e909caa8d13952

hostnames.c: fix scan-build warning
This is breaking scan-build in CI[1]:

    /usr/share/clang/scan-build-14/bin/../libexec/ccc-analyzer [...] -c hostnames.c -o hostnames.o
    hostnames.c:59:10: warning: Null pointer passed to 1st parameter expecting 'nonnull' [core.NonNullParamChecker]
                    return strdup(rv);
                           ^~~~~~~~~~
    1 warning generated.

Likely caused by commit d2802ce60 ("fnettrace cleanup", 2023-07-15).

This also fixes a memory leak of `cmd`.

[1] https://github.com/netblue30/firejail/actions/runs/5568460702/jobs/10171098449

src/fnettrace/hostnames.c
@@ -40,30 +40,32 @@ char *retrieve_hostname(uint32_t ip) {
 		errExit("asprintf");
 
 	FILE *fp = popen(cmd, "r");
-	if (fp) {
-		char *ptr;
-		 if (fgets(buf, MAXBUF, fp)) {
-			ptr = strchr(buf, '\n');
-			if (ptr)
-				*ptr = '\0';
-			if (strncmp(buf, "GeoIP Country Edition:", 22) == 0) {
-				ptr = buf + 22;
-				if (*ptr == ' ' && *(ptr + 3) == ',' &&  *(ptr + 4) == ' ') {
-					rv = ptr + 5;
-					if (strcmp(rv, "United States") == 0)
-						rv = "US";
-				}
+	if (!fp) {
+		geoip_not_found = 1;
+		goto out;
+	}
+
+	char *ptr;
+	if (fgets(buf, MAXBUF, fp)) {
+		ptr = strchr(buf, '\n');
+		if (ptr)
+			*ptr = '\0';
+		if (strncmp(buf, "GeoIP Country Edition:", 22) == 0) {
+			ptr = buf + 22;
+			if (*ptr == ' ' && *(ptr + 3) == ',' && *(ptr + 4) == ' ') {
+				rv = ptr + 5;
+				if (strcmp(rv, "United States") == 0)
+					rv = "US";
 			}
 		}
-		pclose(fp);
-		return strdup(rv);
 	}
-	else
-		geoip_not_found = 1;
+	pclose(fp);
+	if (rv)
+		rv = strdup(rv);
 
+out:
 	free(cmd);
-
-	return NULL;
+	return rv;
 }
 
 void load_hostnames(const char *fname) {
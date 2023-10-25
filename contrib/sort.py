ee27c8e15c73c46a1d4913dc12a207ca803bcbe3
Kelvin M. Klann <kmk3.code@protonmail.com>
2023-10-25 13:55:27 -0300

    sort.py: fix missing/duplicated commands in usage

--- contrib/sort.py
+++ contrib/sort.py
@@ -15,8 +15,8 @@ Usage: {path.basename(argv[0])} [/path/to/profile ...]
 
 The following commands are supported:
 
-    private-bin, private-etc, private-lib, caps.drop, caps.keep, seccomp.drop,
-    seccomp.drop, protocol
+    private-bin, private-etc, private-lib, caps.drop, caps.keep, seccomp,
+    seccomp.drop, seccomp.keep, protocol
 
 Note that this is only applicable to commands that support multiple arguments.
 

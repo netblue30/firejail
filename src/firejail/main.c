6102481cfd9faf33661fb6ed6bca88fd696cafe3
Kelvin M. Klann <kmk3.code@protonmail.com>
2025-08-23 06:56:02 -0300

    add missing malloc error check for fullargv

--- src/firejail/main.c
+++ src/firejail/main.c
@@ -1248,9 +1248,12 @@ int main(int argc, char **argv, char **envp) {
 	}
 	EUID_ASSERT();
 
+	fullargv = malloc(max_arg_count * sizeof(char*));
+	if (!fullargv)
+		errExit("malloc");
+
 	// is this a login shell, or a command passed by sshd,
 	// insert command line options from /etc/firejail/login.users
-	fullargv = malloc(max_arg_count * sizeof(char*));
 	if (*argv[0] == '-' || parent_sshd) {
 		if (argc == 1)
 			login_shell = 1;

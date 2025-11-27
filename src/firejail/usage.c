bd94e87eb79debf0815a73d0d2a0be305ce29332
netblue30 <netblue30@protonmail.com>
2025-11-27 09:57:57 -0500

    sorting xephyr-extra-params

--- src/firejail/usage.c
+++ src/firejail/usage.c
@@ -301,8 +301,9 @@ static const char *const usage_str =
 	"    --x11=xorg - enable X11 security extension.\n"
 	"    --x11=xpra - enable Xpra X11 server.\n"
 	"    --x11=xvfb - enable Xvfb X11 server.\n"
+	"    --xephyr-extra-params=OPTIONS - set Xephyr server command extra parameters\n"
+	"\tfor --x11=xephyr.\n"
 	"    --xephyr-screen=WIDTHxHEIGHT - set screen size for --x11=xephyr.\n"
-	"    --xephyr-extra-params=OPTIONS - set Xephyr server command extra parameters for --x11=xephyr.\n"
 #endif
 	"\n"
 	"Examples:\n"

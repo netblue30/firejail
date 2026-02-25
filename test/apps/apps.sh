01b741aa80eeb8a6034fd1038667a5906ade3263
netblue30 <netblue30@protonmail.com>
2026-02-25 15:14:15 -0500

    RELNOTES and some more testing

--- test/apps/apps.sh
+++ test/apps/apps.sh
@@ -36,7 +36,7 @@ echo "TESTING: pid 1 functionality (test/apps/pid1.exp)"
 
 # X11 apps
 x11apps=(firefox qbittorrent firefox-xephyr galculator libreoffice firefox-xorg \
-		 lowriter gimp inkscape emacs okular kdiff3 gpicview audacity \
+		 lowriter gimp inkscape firefox-neteth emacs okular kdiff3 gpicview audacity \
 		 pauvcontrol mpv dosbox gnome-screenshot \
 		 xterm x11-none xterm-xorg xterm-xephyr xterm-xpra firefox-xpra)
 

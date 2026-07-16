4a5181b2e4e4dff9976fce21ef8f24cf2dccffdd
Kelvin M. Klann <kmk3.code@protonmail.com>
2026-07-16 11:12:35 -0300

    build: testcompile.sh: deduplicate most of the code
    To make it easier to read and maintain.
    
    Misc: This reduces non-empty lines by about 65% (77/224).
    
    Related commits:
    
    * 582c3be85 ("rework make test-compile", 2026-07-15)
    * a3f01f68c ("fixed make clean problem introduced by earlier commit", 2026-07-15)
    * a9b637d54 ("cleanup and fixes for previous commits", 2026-07-16)
    * b6af2b6e7 ("build: testcompile.sh: fix typo of "ERROR"", 2026-07-16)
    
    Relates to #7206.

--- testcompile.sh
+++ testcompile.sh
@@ -7,269 +7,95 @@
 
 # shellcheck source=config.sh
 
+testconfigure() {
+	msg="$1"
+	shift
 
-#*****************************************************************
-printf "default" > testcompile.result
-#*****************************************************************
-make distclean
+	printf '%s' "$msg" >> testcompile.result
+	make distclean
+	./configure "$@" 2>&1 | tee /tmp/testcompile-output
+	if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
+		printf 'TESTING ERROR - %s\n' "$msg"
+		exit 1
+	fi
+}
 
-./configure --enable-fatal-warnings 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - default";
-    exit 1
-fi
+testmake() {
+	msg="$1"
+	shift
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - standard compile";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+	make "$@" 2>&1 | tee /tmp/testcompile-output
+	if grep -E -i 'error:' /tmp/testcompile-output; then
+		printf 'TESTING ERROR - %s\n' "$msg"
+		exit 1
+	fi
 
-#*****************************************************************
-printf "disable dbus proxy configuration" >> testcompile.result
-#*****************************************************************
-make distclean
+	echo " ...OK" >> testcompile.result
+}
 
-./configure --enable-fatal-warnings --disable-dbusproxy 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR -  disable dbus proxy";
-    exit 1
-fi
+: > testcompile.result
+: > /tmp/testcompile-output
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR -  disable dbus proxy";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+msg='default'
+testconfigure "$msg" --enable-fatal-warnings
+testmake "$msg" -j4
 
-#*****************************************************************
-printf "disable chroot configuration" >> testcompile.result
-#*****************************************************************
-make distclean
+msg='disable dbus proxy configuration'
+testconfigure "$msg" --enable-fatal-warnings --disable-dbusproxy
+testmake "$msg" -j4
 
-./configure --enable-fatal-warnings --enable-chroot 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable chroot";
-    exit 1
-fi
+msg='disable chroot configuration'
+testconfigure "$msg" --enable-fatal-warnings --enable-chroot
+testmake "$msg" -j4
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable chroot";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+msg='disable user namespace configuration'
+testconfigure "$msg" --enable-fatal-warnings --disable-userns
+testmake "$msg" -j4
 
-#*****************************************************************
-printf "disable user namespace configuration" >> testcompile.result
-#*****************************************************************
-make distclean
+msg='disable network namespace configuration'
+testconfigure "$msg" --enable-fatal-warnings --disable-network
+testmake "$msg" -j4
 
-./configure --enable-fatal-warnings --disable-userns 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable user namespace";
-    exit 1
-fi
+msg='disable X11 support'
+testconfigure "$msg" --enable-fatal-warnings --disable-x11
+testmake "$msg" -j4
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable user namespace";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+msg='enable selinux'
+testconfigure "$msg" --enable-fatal-warnings --enable-selinux
+testmake "$msg" -j4
 
-#*****************************************************************
-printf "disable network namespace configuration" >> testcompile.result
-#*****************************************************************
-make distclean
+msg='disable file transfer'
+testconfigure "$msg" --enable-fatal-warnings --disable-file-transfer
+testmake "$msg" -j4
 
-./configure --enable-fatal-warnings --disable-network 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable network namespace";
-    exit 1
-fi
+msg='enable apparmor'
+testconfigure "$msg" --enable-fatal-warnings --enable-apparmor
+testmake "$msg" -j4
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable network namespace";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+msg='disable landlock'
+testconfigure "$msg" --enable-fatal-warnings --disable-landlock
+testmake "$msg" -j4
 
-#*****************************************************************
-printf "disable X11 support" >> testcompile.result
-#*****************************************************************
-make distclean
+msg='disable output logging'
+testconfigure "$msg" --enable-fatal-warnings --disable-output
+testmake "$msg" -j4
 
-./configure --enable-fatal-warnings --disable-x11 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable X11 support";
-    exit 1
-fi
+msg='disable private-lib'
+testconfigure "$msg" --enable-fatal-warnings --disable-private-lib
+testmake "$msg" -j4
 
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable X11 support";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
+msg='enable-only-syscfg-profiles'
+testconfigure "$msg" --enable-fatal-warnings --enable-only-syscfg-profiles
+testmake "$msg"
 
-#*****************************************************************
-printf "enable selinux" >> testcompile.result
-#*****************************************************************
-make distclean
+msg='enable force nonewprivs'
+testconfigure "$msg" --enable-fatal-warnings --enable-force-nonewprivs
+testmake "$msg" -j4
 
-./configure --enable-fatal-warnings --enable-selinux 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable selinux";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable selinux";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "disable file transfer" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --disable-file-transfer 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable file transfer";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable file transfer";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "enable apparmor" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --enable-apparmor 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable apparmor";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable apparmor";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "disable landlock" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --disable-landlock 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable landlock";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable landlock";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "disable output logging" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --disable-output 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable output logging";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable output logging";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "disable private-lib" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --disable-private-lib 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable private-lib";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - disable private-lib";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-
-#*****************************************************************
-printf "enable-only-syscfg-profile" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings --enable-only-syscfg-profiles 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR --enable-only-syscfg-profile";
-    exit 1
-fi
-
-make 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR --enable-only-syscfg-profile";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-#*****************************************************************
-printf "enable force nonewprivs" >> testcompile.result
-#*****************************************************************
-make distclean
-
-./configure --enable-fatal-warnings  --enable-force-nonewprivs 2>&1 | tee /tmp/testcompile-output
-if grep -E '(WARNING|ERROR)' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable force nonewprivs";
-    exit 1
-fi
-
-make -j4 2>&1 | tee /tmp/testcompile-output
-if grep -E -i 'error:' /tmp/testcompile-output; then
-    echo "TESTING ERROR - enable force nonewprivs";
-    exit 1
-fi
-echo " ...OK" >> testcompile.result
-
-
-#*****************************************************************
 echo "cleanup" >> testcompile.result
-#*****************************************************************
 make distclean
 rm /tmp/testcompile-output
+
 echo "*******************************************"
 echo "All fine!!!" >> testcompile.result
 cat testcompile.result
-
-
-
